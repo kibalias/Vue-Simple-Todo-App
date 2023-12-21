bb3db8676114859ae76e22032c97ee0c9121e8f2
kibalias <74294078+kibalias@users.noreply.github.com>
2023-12-22 02:13:57 +0800

    updating status upon btn click

--- todo-app/src/components/TaskList.vue
+++ todo-app/src/components/TaskList.vue
@@ -1,7 +1,17 @@
 <script setup>
+import { ref } from 'vue';
 // define props
-const props = defineProps(['tasks'])
+const props = defineProps(['tasks']);
+const isMarkedDone = ref('false');
 
+const updateListStatus = (index) => {
+    // get selected task's index and set the status
+    if(props.tasks[index].status === 'Ongoing') {
+        props.tasks[index].status = 'Completed';
+    } else {
+        props.tasks[index].status = 'Ongoing';
+    }
+}
 </script>
 
 <template>
@@ -14,11 +24,17 @@ const props = defineProps(['tasks'])
             </thead>
             <tbody>
                 <tr v-for="(task, index) in tasks" :key="index">
-                    <td>{{task.name}}</td>
-                    <td>{{task.status}}</td>
+                    <td :class="(task.status === 'Completed') ? 'markedDone' : '' ">{{task.name}}</td>
+                    <td :class="(task.status === 'Completed') ? 'statusDone' : '' ">{{task.status}}</td>
                     <td>
                         <div class="tableBtn">
-                            <button class="doneBtn">Mark as Done</button>
+                            <!-- TO DO:
+                                set function that when doneBtn is clicked will change the text inside 
+                            -->
+                            <button class="doneBtn" @click="updateListStatus(index)">
+                                <span v-if="(task.status === 'Completed')">Mark as Undone</span>
+                                <span v-else>Mark as Done</span>
+                            </button>
                             <button class="delete">Delete</button>
                         </div>
                     </td>
