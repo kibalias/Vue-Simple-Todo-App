accc0f6b003c3d0e31b27e6ef37d422a88b42ffa
kibalias <74294078+kibalias@users.noreply.github.com>
2023-12-21 23:36:00 +0800

    adding task view and methods

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,47 +1,38 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import AddTask from './components/AddTask.vue'
+import TaskList from './components/TaskList.vue'
+import { ref } from 'vue';
+
+const tasks = ref([]);
+tasks.value = [
+  {
+  name: 'Create a simple todo app with Vue',
+  status: 'Ongoing'
+  },
+  {
+    name: 'Learn Laravel',
+    status: 'Ongoing'
+  },
+  {
+    name: 'Learn REST API',
+    status: 'Ongoing'
+  },
+]
+
+const handleAddingTask = (addedTask) => {
+  tasks.value.push({
+    name: addedTask,
+    status: 'Ongoing'
+  })
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div class="container">
+      <AddTask @addingTask="handleAddingTask"/>
+      <TaskList :tasks=tasks />
+    </div>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ todo-app/src/components/AddTask.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+const newTask = ref('');
+
+const emit = defineEmits(['addingTask']);
+
+const onSubmit = () => {
+    if(!newTask.value){
+        alert('Please add a task in the field!');
+        return;
+    }
+
+    const addedTask = newTask.value;
+    // pass the value of the input field to the parent component
+
+    emit('addingTask', addedTask);
+    newTask.value = ''
+}
+</script>
+
+<template>
+    <div>
+        <h1>A Todo App</h1>
+        <form @submit.prevent="onSubmit">
+            <div class="content-container">
+                <input type="text" v-model="newTask" placeholder="Enter the task"/>
+                <button class="addTaskBtn">Add Task</button>
+            </div>
+            
+        </form>
+    </div>
+</template>
